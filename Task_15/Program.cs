50a1b1520a7691b5193c1609b01f5ced88018e6e
KAMaz64 <konstantin.a.mazunin@gmail.com>
2022-12-24 12:09:48 +0300

    Amendments to Task_15 code

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -8,7 +8,7 @@ Console.Clear();
 Console.WriteLine("Введите порядковый номер дня недели (от 1 до 7)");
 int N = int.Parse(Console.ReadLine());
 
-if (N<1 && N>7)
+if (N<1 || N>7)
 {
     Console.WriteLine("Ошибка ввода данных");
 }
